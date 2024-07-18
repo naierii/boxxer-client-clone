204f4d2a84d3e00fd73778a19c297eec0323ec2b
developmentexpert121 <developmentexpert121@gmail.com>
2024-07-18 15:39:06 +0530

    Update api path

--- projects/environments/environment.ts
+++ projects/environments/environment.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
   production: true,
-  host: 'http://167.172.60.145:3030',
-  api_url: 'http://167.172.60.145:3030/',
+  host: 'https://api.skyeathletic.com',
+  api_url: 'https://api.skyeathletic.com/',
   site: 'https://localhost:4200',
   image_cdn: 'https://img.boxxerworld.com',
   stripe_key: 'pk_test_gm5ngDkM9un8t17TjhnvqhLh',
