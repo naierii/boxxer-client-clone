acf1d23171bc973e33ec165c589be824cbbf7301
developmentexpert121 <developmentexpert121@gmail.com>
2024-07-18 15:34:12 +0530

    Update api pdath

--- projects/environments/environment.prod.ts
+++ projects/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  host: 'http://167.172.60.145:3030',
-  api_url: 'http://167.172.60.145:3030/',
+  host: 'https://api.skyeathletic.com',
+  api_url: 'https://api.skyeathletic.com/',
   site: 'https://206.189.118.244',
   image_cdn: 'https://img.boxxerworld.com',
   stripe_key: 'pk_test_gm5ngDkM9un8t17TjhnvqhLh',
