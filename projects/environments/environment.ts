231f5edf97cacd16d0498dfb979a69d7736cc75a
developmentexpert121 <developmentexpert121@gmail.com>
2024-07-22 13:14:06 +0530

    Dynamic the content

--- projects/environments/environment.ts
+++ projects/environments/environment.ts
@@ -4,12 +4,12 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   host: 'https://api.skyeathletic.com',
   api_url: 'https://api.skyeathletic.com/',
-  site: 'https://localhost:4200',
+  site: 'https://skyeathletic.com',
   image_cdn: 'https://img.boxxerworld.com',
   stripe_key: 'pk_test_gm5ngDkM9un8t17TjhnvqhLh',
-  admin: 'https://localhost:4201',
-  domain: 'localhost',
+  admin: 'https://admin.skyeathletic.com',
+  domain: '.skyeathletic.com',
 };
