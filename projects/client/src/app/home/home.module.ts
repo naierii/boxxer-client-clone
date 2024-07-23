98d5fc11dcd46249bcb04fb7bf1c4a1cc6efe27b
developmentexpert121 <developmentexpert121@gmail.com>
2024-07-23 17:11:06 +0530

    added review loader

--- projects/client/src/app/home/home.module.ts
+++ projects/client/src/app/home/home.module.ts
@@ -4,9 +4,8 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '@app/shared/shared.module';
 import { CarouselModule } from 'ngx-owl-carousel-o';
-import { NgCircleProgressModule } from 'ng-circle-progress';
 @NgModule({
-  imports: [SharedModule, HomeRoutingModule,CarouselModule,NgCircleProgressModule.forRoot()],
+  imports: [SharedModule, HomeRoutingModule,CarouselModule],
   declarations: [HomeComponent]
 })
 export class HomeModule {}
