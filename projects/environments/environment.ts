8b83cae889fa9bfe479d9896a4afd87cd42a97a5
developmentexpert121 <developmentexpert121@gmail.com>
2024-08-13 17:21:52 +0530

    update domain

--- projects/environments/environment.ts
+++ projects/environments/environment.ts
@@ -11,5 +11,5 @@ export const environment = {
   image_cdn: 'https://img.boxxerworld.com',
   stripe_key: 'pk_test_gm5ngDkM9un8t17TjhnvqhLh',
   admin: 'https://admin.skyeathletic.com',
-  domain: '.skyeathletic.com',
+  domain: 'skyeathletic.com',
 };
