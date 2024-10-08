06b6540ff2b15db210f0c2bcb45faf699acc4787
developmentexpert121 <developmentexpert121@gmail.com>
2024-10-08 17:13:00 +0530

    added qr code

--- projects/client/src/app/core/components/footer/footer.component.ts
+++ projects/client/src/app/core/components/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { footerNav3 } from './../../_menu-footer3';
 @Component({
   selector: 'bx-footer, [bx-footer]',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
   @Input()
@@ -16,4 +16,10 @@ export class FooterComponent {
   public footerNav3 = footerNav3;
 
   constructor() {}
+  shareViaEmail() {
+    window.open(
+      'mailto:?subject=Check%20this%20QR%20Code&body=Here%20is%20a%20QR%20code%20you%20might%20need:%20[<img src="https://res.cloudinary.com/dot-to-dot-design/w_868,f_auto,q_auto/box-img/content/1545046342086/boxxerworld-christmas-gift.jpg" />]',
+      '_blank'
+    );
+  }
 }
